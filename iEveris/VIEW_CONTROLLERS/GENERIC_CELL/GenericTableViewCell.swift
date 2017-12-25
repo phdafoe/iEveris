e8e04dc081e7506ce25a9317c30e6d53d18dc26d
Andres <andres@MacBook-Pro-de-Andres.local>
2017-12-25 15:31:53 +0100

    +[iOS] - Ajuste de imágenes y de Celda genérica -

---
+++ iEveris/VIEW_CONTROLLERS/GENERIC_CELL/GenericTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GenericTableViewCell.swift
+//  iEveris
+//
+//  Created by Andres on 25/12/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import UIKit
+
+class GenericTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
